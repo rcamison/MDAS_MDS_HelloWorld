429f95a17f814c525ff1fde0b3d2952bc20904ea
rcamison <85695417+rcamison@users.noreply.github.com>
2022-03-26 17:45:10 +0100

    Acceptance test

---
+++ tests/HelloWorldTests/HelloWorldTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using Xunit;
+
+namespace HelloWorldTests;
+public class HelloWorldTest
+{
+    private static HttpClient client = new HttpClient();
+
+    [Fact, Trait("Type", "Acceptance")]
+    private void Should_Get_A_Pokemon_By_Its_Id()
+    {
+        //Given
+        HttpClient httpClient = new HttpClient();
+        string getUrl = "http://localhost:5061/helloworld";
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, getUrl);
+
+        //When
+        var response = httpClient.SendAsync(request).GetAwaiter().GetResult();
+
+        //Then
+        Assert.True(response.IsSuccessStatusCode);
+    }
+}
